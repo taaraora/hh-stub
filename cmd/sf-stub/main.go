59c90c54404b898620b6bf291b702f28ba3e09f3
Dmytro Tsapko <dmytro.tsapko@gmail.com>
2022-07-12 14:47:45 +0300

    tls

--- cmd/sf-stub/main.go
+++ cmd/sf-stub/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"context"
+	"crypto/tls"
 	"fmt"
 	"github.com/taaraora/hh-stub/pkg/hh"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/keepalive"
 	"log"
 	"time"
@@ -17,8 +19,14 @@ func main() {
 		//PermitWithoutStream: cfg.PermitWithoutStream,
 	}
 
+	config := &tls.Config{
+		//Certificates: []tls.Certificate{serverCert},
+		ClientAuth:         tls.NoClientCert,
+		InsecureSkipVerify: true,
+	}
+
 	opts := []grpc.DialOption{
-		grpc.WithInsecure(),
+		grpc.WithTransportCredentials(credentials.NewTLS(config)),
 		grpc.WithKeepaliveParams(keepaliveCfg),
 		//grpc.WithDefaultCallOptions(
 		//	grpc.MaxCallRecvMsgSize(cfg.MaxMessageSize),
@@ -29,7 +37,8 @@ func main() {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	serverURI := fmt.Sprintf("localhost:%d", 8080)
+	//serverURI := fmt.Sprintf("localhost:%d", 8080)
+	serverURI := fmt.Sprintf("hh.nms.linnaeus.freedomfi.com:%d", 443)
 
 	conn, err := grpc.DialContext(ctx, serverURI, opts...)
 	if err != nil {
@@ -45,8 +54,8 @@ func main() {
 	req := &hh.ReportSessionsStatsRequest{
 		SessionStats: []*hh.SessionStats{
 			&hh.SessionStats{
-				Sid: "fd",
-				Usage: []*hh.SessionStatsUsage{},
+				Sid:                   "fd",
+				Usage:                 []*hh.SessionStatsUsage{},
 				Imsi:                  "we",
 				Imei:                  "g",
 				Msisdn:                "weg",
@@ -75,13 +84,19 @@ func main() {
 		},
 	}
 
+	n := 1
+
 	for {
 		time.Sleep(time.Second * 5)
+		reqCtx := context.Background()
+		reqCtx, _ = context.WithTimeout(reqCtx, time.Second*5)
 
-		_, err := client.ReportSessionsStats(ctx, req)
+		_, err := client.ReportSessionsStats(reqCtx, req)
 		if err != nil {
 			log.Printf("%v", err.Error())
 		}
 
+		fmt.Printf("the %vth message was sent\n", n)
+		n++
 	}
 }
